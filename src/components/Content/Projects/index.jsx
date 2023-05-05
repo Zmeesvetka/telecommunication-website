97b7f0cac9ba8397c0814ee4c0df8ace2ee84440
Zmeesvetka <105983414+Zmeesvetka@users.noreply.github.com>
2023-05-05 23:36:58 +0700

    Add base Project block

--- src/components/Content/Projects/index.jsx
+++ src/components/Content/Projects/index.jsx
@@ -1,10 +1,47 @@
 import React from 'react';
 
-export default class Projects extends React.Component {
+import { GiAbstract070, GiAbstract116, GiBoltShield, GiBoltSpellCast } from "react-icons/gi";
+
+import ProjectBlock from './ProjectBlock';
+
+export default class ProjectList extends React.Component {
+    projects = [
+        {
+            icon: GiAbstract070,
+            direction: 'Lorem',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+        },
+        {
+            icon: GiAbstract116,
+            direction: 'Ipsum dolor',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+        },
+        {
+            icon: GiBoltShield,
+            direction: 'Sit amet',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+        },
+        {
+            icon: GiBoltSpellCast,
+            direction: 'Consectetur',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+        }
+    ];
 
     render() {
         return (
-            <></>
-        )
+            <section id="Projects" className="px-2 clean-block features">
+                <div className="container py-20">
+                    <div className="mb-8">
+                        <p className="text-3xl font-medium text-companyBlue-600 text-center">Projects</p>
+                    </div>
+                    <div className="flex flex-wrap justify-center">
+                        {
+                            this.projects.map((project, index) => <ProjectBlock entity={project} key={index}/>)
+                        }
+                    </div>
+                </div>
+            </section>
+        );
     }
 }
